7a8189ec9f65ffe110615f659ba8cf35222ebd54
nucleus-of-an-atom <97610766+nucleus-of-an-atom@users.noreply.github.com>
2022-04-03 19:21:45 +0545

    Update: New demonstration

--- main/main.py
+++ main/main.py
@@ -5,30 +5,27 @@ from conf.conf import *
 from tk import intp as intp
 from tk.intp import *
 sys.path.remove("../")
-
-# gui
-config=read("config.conf")
-btn2_lbl=find(config,"btn2","label")
-btn3_lbl=find(config,"btn3","label")
-
-Gui.__style__("style.css")
 g = vars(intp)
-pkg=["pack_start",True,True,0]
-Win("root",[],
-    Box("hbox",pkg,["orientation=Vars.hori"],
-        Box("vbox1",pkg,["orientation=Vars.vert"],
-            Btn("btn1",["label='btn1'"]),
+
+#variables
+
+#gui
+Win("win",[],
+    HeaderBar("hb",[],True,
+        Box("hbCenter",Vars.pks,["orientation=Vars.hori"],
+            SE("hbSearch",[])
         ),
-        Box("vbox2",pkg,["orientation=Vars.vert","spacing=10"],
-            Btn("btn2",[f"label='{btn2_lbl}'"]),
-            Btn("btn3",[f"label='{btn3_lbl}'"]),
-        )
+        Label("hbLabelLeft",["label='NEWS!'"])
     ),
+    Box("root",Vars.pks,["orientation=Vars.vert"],
+        Label("news",["label='Ive made more changes :)'"])
+    )
 )
-click_count=0
-def btn1_click(w):
-    global click_count
-    click_count+=1
-    w.set_label(f"You clicked me! You clicked me {click_count} times!")
-g["btn1"].connect("clicked",btn1_click)
-Gui.__run__(g["root"])
+
+#after interpret
+g["hbSearch"].set_placeholder_text("Type to search")
+Funcs._Class(g["hbSearch"],"hbSearch")
+Funcs._Class(g["news"],"news")
+
+Gui.__style__("./style.css")
+Gui.__run__(g["win"])
